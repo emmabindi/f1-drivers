4e68224540833a3efd57dd35a31c2dc0a72fda0f
Emma Bindi <emma.bindi@outlook.com>
2020-04-21 18:44:41 +1000

    bootstrap styling added and updated links

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,91 +1,76 @@
 class DriversController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+  before_action :find_driver, only: [:show, :edit, :update, :destroy]
+
   def index
-    @drivers = Driver.all
+    @drivers = Driver.all.order(:id)
   end
 
   def new
-    # harrison had this in here but i didn't see it all 
-    # @driver = Driver.new?
+    @driver = Driver.new
   end
   
   #Show single driver
   def show
-    id = params[:id].to_i
-    @driver = Driver.find(id)
+    # id = params[:id].to_i
+    # @driver = Driver.find(id)
   end
 
   #Create a new driver
   def create
     @driver = Driver.create(
-      name: params[:driver][:name],
-      team: params[:driver][:team],
-      profile: params[:driver][:profile]
-    )
+      name: driver_params[:name],
+      team: driver_params[:team],
+      profile: driver_params[:profile])
     redirect_to @driver 
+    
+    # long version - before driver params method created 
+    # @driver = Driver.create(
+    #   name: params[:driver][:name],
+    #   team: params[:driver][:team],
+    #   profile: params[:driver][:profile])
+
+    # short version:
+    # @driver = Driver.create(driver_params)
     # or redirect_to driver_path(@driver)
   end
 
   #page to enter data about updating a driver
   def edit
     id = params[:id].to_i
-    @driver = Driver.find(id)
+    # @driver = Driver.find(id)
   end
 
   #udpate a driver
   def update 
-    id = params[:id].to_i
-    @driver = Driver.find(id)
-    @driver.update(
-      name: params[:driver][:name],
-      team: params[:driver][:team],
-      profile: params[:driver][:profile]
-    )
+    @driver.update(driver_params)
+    redirect_to driver_path(@driver.id)
+    # id = params[:id].to_i
+    # # @driver = Driver.find(id)
+    # @driver.update(
+    #   name: params[:driver][:name],
+    #   team: params[:driver][:team],
+    #   profile: params[:driver][:profile])
+    # redirect_to @driver 
   end
 
   # remove a driver
   def destroy
-    @driver = Driver.find(params[:id])
+    # @driver = Driver.find(params[:id])
     @driver.destroy
 
     redirect_to drivers_path
   end
+
+  private 
+
+  def driver_params
+    params.require(:driver).permit(:name, :team, :profile)
+  end
+
+  def find_driver
+    @driver = Driver.find(params[:id])
+  end
+
 end
-
-
-# MY OLD CODE
-
-#   def index
-#   # render json: @drivers
-#   end
-
-#   #Show single driver
-#   def show
-#     # @drivers = Driver.find(params[:id])
-#   end
-
-#   #Create a new driver
-#   def create
-#     # @driver = Driver.new(params[:driver])
-
-#     # @driver.save
-#     # redirect_to @driver
-
-#     # or 
-
-#     # @drivers << {
-#     #   name: params[:name],
-#     #   team: params[:team],
-#     #   profile: params[:profile]
-#     # }
-#   end
-
-#   # #Update a driver
-#   # def update
-#   # end
-
-#   # #Remove a driver
-#   # def destroy
-#   # end
-# end
